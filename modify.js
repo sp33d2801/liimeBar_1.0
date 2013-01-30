4ea0431dc96314468b0b7751d88304a8a1a20783
Liem.LeHieu <sp33d2801@yahoo.com>
2013-01-30 11:00:06 +0700

    liem test
    liem test

---
+++ modify.js
@@ -0,0 +1,12 @@
+$(function() {
+  $('body').liimeBar({
+    hideBar: false,
+    hideButton: false,
+    scrollSpeed: 20,
+    barColour: '#888888',
+    barOpacity: 0.3,
+    buttonColour: '#000000',
+    buttonOpacity: 0.3,
+    loopCheck: true
+  });
+});
